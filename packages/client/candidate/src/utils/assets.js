df4b637fd4e13a78447974f499fb6d01c0dd595e
Matt Searle <mattsearle@ymail.com>
2019-07-03 00:23:55 +0100

    feat: Add font awesome icons
    re #12

---
+++ packages/client/candidate/src/utils/assets.js
@@ -0,0 +1,32 @@
+import {
+	faCheck,
+	faCheckCircle,
+	faInfoCircle,
+	faExclamationTriangle,
+	faExclamationCircle,
+	faArrowUp,
+	faAngleRight,
+	faAngleLeft,
+	faAngleDown,
+	faEye,
+	faEyeSlash,
+	faCaretDown,
+	faCaretUp
+} from '@fortawesome/free-solid-svg-icons';
+
+// Icons used internally within Buefy
+export const internalIcons = {
+	faCheck,
+	faCheckCircle,
+	faInfoCircle,
+	faExclamationTriangle,
+	faExclamationCircle,
+	faArrowUp,
+	faAngleRight,
+	faAngleLeft,
+	faAngleDown,
+	faEye,
+	faEyeSlash,
+	faCaretDown,
+	faCaretUp
+};
