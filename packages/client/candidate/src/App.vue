0a56e85155373876da2d7dafddbdd483c678d19f
Matt Searle <msearle@scottlogic.com>
2019-07-02 09:08:11 +0100

    feat(candidate-app): Mock some components together
    re #12

--- packages/client/candidate/src/App.vue
+++ packages/client/candidate/src/App.vue
@@ -1,17 +1,16 @@
 <template>
 	<div id="app">
-		<section class="section">
-			<div class="container">
-				<h1 class="title">
-					Section
-				</h1>
-				<h2 class="subtitle">
-					A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
-				</h2>
-			</div>
-		</section>
+		<ApplicationPage />
 	</div>
 </template>
+
+<script>
+import ApplicationPage from './views/ApplicationPage';
+
+export default {
+	components: { ApplicationPage }
+};
+</script>
 
 <style>
 #app {
---
+++ packages/client/candidate/src/components/ApplicationForm.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="container">
+		<section>
+			<BField v-model="firstName" label="First Name">
+				<BInput />
+			</BField>
+			<BField v-model="lastName" label="Last Name">
+				<BInput />
+			</BField>
+			<BField v-model="email" label="Email">
+				<BInput type="email" maxlength="30" />
+			</BField>
+		</section>
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		labelPosition: 'on-border',
+		firstName: '',
+		lastName: '',
+		email: ''
+	})
+};
+</script>
+
+<style></style>
