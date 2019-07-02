332f6cc281a1380a850202bdb4a92fb1e704a712
Matt Searle <mattsearle@ymail.com>
2019-07-03 00:24:49 +0100

    feat(candidate-ui): Add form inputs
    re #12

--- packages/client/candidate/src/components/ApplicationForm.vue
+++ packages/client/candidate/src/components/ApplicationForm.vue
@@ -1,28 +1,62 @@
 <template>
 	<div class="container">
-		<section>
-			<BField v-model="firstName" label="First Name">
-				<BInput />
-			</BField>
-			<BField v-model="lastName" label="Last Name">
-				<BInput />
-			</BField>
-			<BField v-model="email" label="Email">
-				<BInput type="email" maxlength="30" />
-			</BField>
+		<section class="section">
+			<form>
+				<BField v-model="firstName" label="First Name">
+					<BInput placeholder="Alex" />
+				</BField>
+				<BField v-model="lastName" label="Last Name">
+					<BInput placeholder="Smith" />
+				</BField>
+				<BField v-model="email" label="Email">
+					<BInput type="email" placeholder="alex@example.com" icon="envelope" />
+				</BField>
+				<BField v-model="phoneNumber" label="Phone Number">
+					<BInput placeholder="07123456789" icon="phone" />
+				</BField>
+				<BField v-model="postCode" label="Post Code">
+					<BInput icon="mail-bulk" />
+				</BField>
+				<BField v-model="role" label="Role">
+					<BInput placeholder="" />
+				</BField>
+				<BField v-model="currentSalary" label="Current Salary">
+					<BInput placeholder="" />
+				</BField>
+				<BField v-model="expectedSalary" label="Expected Salary">
+					<BInput placeholder="" />
+				</BField>
+				<FileUpload v-model="cv" file-type="cv" />
+			</form>
 		</section>
 	</div>
 </template>
 
 <script>
+import FileUpload from './FileUpload';
+
 export default {
+	components: { FileUpload },
 	data: () => ({
 		labelPosition: 'on-border',
 		firstName: '',
 		lastName: '',
-		email: ''
+		email: '',
+		postCode: '',
+		phoneNumber: '',
+		role: '',
+		currentSalary: '',
+		expectedSalary: '',
+		cv: {}
 	})
 };
 </script>
 
-<style></style>
+<style>
+form {
+	width: 66%;
+	min-width: 200px;
+	max-width: 800px;
+	margin: auto;
+}
+</style>
